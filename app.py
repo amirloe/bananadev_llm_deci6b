55e86ff3d1007bb6ea6e12e6e96ab5911a47f714
Amir <amir@argmaxml.com>
2023-08-21 15:56:21 +0300

    flan_t5 imp

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+import numpy as np
 from potassium import Potassium, Request, Response
-
-from transformers import pipeline
+from transformers import T5Tokenizer, T5Model, T5ForConditionalGeneration
 import torch
 
 app = Potassium("my_app")
@@ -8,11 +8,13 @@ app = Potassium("my_app")
 # @app.init runs at startup, and loads models into the app's context
 @app.init
 def init():
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
+    model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base", device=device)
    
     context = {
-        "model": model
+        "model": model,
+        "tokenizer": tokenizer
     }
 
     return context
@@ -20,12 +22,24 @@ def init():
 # @app.handler runs for every call
 @app.handler()
 def handler(context: dict, request: Request) -> Response:
-    prompt = request.json.get("prompt")
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    prompts = request.json.get("prompts")
+    word_list = request.json.get("word_list")
     model = context.get("model")
-    outputs = model(prompt)
+    tokenizer = context.get("tokenizer")
+    tokens = [tokenizer.encode(word)[0] for word in word_list]
+
+    input_ids = tokenizer(prompts, return_tensors="pt", padding=True).input_ids.to(device)
+    decoder_input_ids = tokenizer(prompts, return_tensors="pt", padding=True).input_ids
+    decoder_input_ids = model._shift_right(decoder_input_ids).to(device)
+    output = model(input_ids, decoder_input_ids=decoder_input_ids)
+    logits = output.logits.to("cpu").detach().numpy()
+    words_logits = logits[:, 0, tokens]
+    output = np.argmax(words_logits, axis=1)
 
     return Response(
-        json = {"outputs": outputs[0]}, 
+        json = {"logits": words_logits.tolist(),
+                "output": output.tolist()},
         status=200
     )
 
