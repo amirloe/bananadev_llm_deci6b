3b741555d6e54d92d6325108a970b59e0443751e
Amir Loewenthal <amir@argmaxml.com>
2023-08-31 17:42:47 +0300

    fix

--- download.py
+++ download.py
@@ -3,11 +3,15 @@
 
 # In this example: A Huggingface BERT model
 
-from transformers import pipeline
+from transformers import T5Tokenizer, T5Model, T5ForConditionalGeneration
+from sentence_transformers import SentenceTransformer
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+    T5Tokenizer.from_pretrained("google/flan-t5-base")
+    T5ForConditionalGeneration.from_pretrained("google/flan-t5-base", device_map="auto")
+    SentenceTransformer('BAAI/bge-small-en')
+
 
 if __name__ == "__main__":
     download_model()
